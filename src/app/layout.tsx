a3a0383ff42c6e0fbc2203a5fba6aaef623d4e02
evanvidel <evanvidel@gmail.com>
2023-10-23 22:02:15 -0300

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Next E--Commerce 13',
+  description: 'Next E--Commerce utilizandoma versão 13',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
